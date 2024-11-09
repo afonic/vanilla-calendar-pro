38a1aabcb228e5261100a768a7cb2b40d4e5757c
Iosif Chatzimichail <afonic@users.noreply.github.com>
2024-11-09 13:00:22 +0200

    Use CSS variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,30 @@ module.exports = {
         'light-mode': 'linear-gradient(145deg, rgb(6 182 212 / 4%) 12%, rgb(6 182 212 / 10%) 42%, rgb(6 182 212 / 5%) 60%, rgb(6 182 212 / 18%) 85%)',
         'dark-mode': 'linear-gradient(145deg, rgb(6 182 212 / 0%) 12%, rgb(6 182 212 / 3%) 42%, rgb(6 182 212 / 10%) 60%, rgb(6 182 212 / 4%) 85%)',
       },
+      colors: {
+        'calendar': {
+          'white': 'var(--calendar-color-white)',
+          'slate': {
+            50: 'var(--calendar-color-slate-50)',
+            100: 'var(--calendar-color-slate-100)',
+            200: 'var(--calendar-color-slate-200)',
+            300: 'var(--calendar-color-slate-300)',
+            400: 'var(--calendar-color-slate-400)',
+            500: 'var(--calendar-color-slate-500)',
+            600: 'var(--calendar-color-slate-600)',
+            900: 'var(--calendar-color-slate-900)',
+          },
+          'main': 'var(--calendar-color-main)',
+          'weekend': {
+            'enabled': 'var(--calendar-color-weekend-enabled)',
+            'disabled': 'var(--calendar-color-weekend-disabled)',
+          },
+          'time': {
+            'enabled': 'var(--calendar-color-time-enabled)',
+            'focus': 'var(--calendar-color-time-focus)',
+          }
+        },
+      },
     },
   },
   corePlugins: {
